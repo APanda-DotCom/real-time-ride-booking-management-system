f8d0c8cbf3b6822f50815e3e43f04bc2381864ff
APanda-DotCom <ashutoshpanda843@gmail.com>
2025-12-12 18:54:38 +0530

    added db.js and modified app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,10 @@
 const dotenv = require('dotenv');
 dotenv.config();
-
 const express = require('express');
+const connectToDb =require('./db/db');
+connectToDb();
+
+
 const app = express();
 
 const cors = require('cors');
