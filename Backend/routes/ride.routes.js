8707a320a5075980c7f16e87e6887e0c1ada1793
APanda-DotCom <ashutoshpanda843@gmail.com>
2026-03-06 21:01:49 +0530

    Add Backend rideroute

--- Backend/routes/ride.routes.js
+++ Backend/routes/ride.routes.js
@@ -1,31 +1,56 @@
 const express = require('express');
-const router = express.Router();   
-const { body } = require('express-validator');
+const router = express.Router();
+const { body, query } = require('express-validator');
 const rideController = require('../controllers/ride.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
+
+
+
+
+// CREATE RIDE
 router.post(
   '/create',
-  body('userId')
-    .isString()
-    .isLength({ min: 24, max: 24 })
-    .withMessage('invalid user id'),
+  authMiddleware.authUser,
+  body('pickup').isString().isLength({ min: 3 }).withMessage('Invalid pickup address'),
+  body('destination').isString().isLength({ min: 3 }).withMessage('Invalid destination address'),
+  body('vehicleType').isIn(['auto', 'car', 'carXL', 'motorcycle']).withMessage('Invalid vehicle type'),
+  rideController.createRide
+);
 
-  body('pickup')
-    .isString()
-    .isLength({ min: 3 })
-    .withMessage('invalid pickup address'),
+// GET FARE
+router.get(
+  '/get-fare',
+  authMiddleware.authUser,
+  query('pickup').isString().isLength({ min: 3 }).withMessage('Invalid pickup address'),
+  query('destination').isString().isLength({ min: 3 }).withMessage('Invalid destination address'),
+  rideController.getFare
+);
 
-  body('destination')
-    .isString()
-    .isLength({ min: 3 })
-    .withMessage('invalid destination address'),
+// CONFIRM RIDE
+router.post(
+  '/confirm',
+  authMiddleware.authCaptain,
+  body('rideId').isMongoId().withMessage('Invalid ride id'),
+  //body('otp').isString().isLength({ min: 4, max: 4 }).withMessage('Invalid OTP'),
+  rideController.confirmRide
+);
 
-  body('vehicleType')
-    .isString()
-    .isIn(['auto', 'car', 'motorcycle'])
-    .withMessage('invalid vehicle type'),
+// START RIDE
+router.post(
+  '/start-ride',
+  authMiddleware.authCaptain,
+  body('rideId').isMongoId().withMessage('Invalid ride id'),
+  body('otp').isString().isLength({ min: 4, max: 4 }).withMessage('Invalid OTP'),
+  rideController.startRide
+);
 
-  rideController.createRide   
+// END RIDE
+router.post(
+  '/end-ride',
+  authMiddleware.authCaptain,
+  body('rideId').isMongoId().withMessage('Invalid ride id'),
+  rideController.endRide
 );
 
 module.exports = router;
